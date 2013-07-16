775a7b984e5e94f0334260eb558baa6b7e5358bf
Jože Fabčič <joze.fabcic@cosylab.com>
2013-07-16 14:30:59 +0200

    updated icons

--- www/common.js
+++ www/common.js
@@ -1,5 +1,5 @@
 var DATA_URL = "http://www.moidom.si/server_prod/";
-DATA_URL = "http://www.moidom.si/server_test/";
+//DATA_URL = "http://www.moidom.si/server_test/";
 //DATA_URL = "http://localhost:8080/server_prod/"
 var HTML_URL = ""; // useful for tracking the code that changes URLs
 
